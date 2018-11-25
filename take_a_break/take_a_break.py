4fb0f6c000e77a33d1ecc75ccba0ff2857287117
Saurav Jaiswal <sauravjaiswal999@gmail.com>
2018-11-25 15:17:11 +0530

    Third Commit

--- take_a_break/take_a_break.py
+++ take_a_break/take_a_break.py
@@ -5,14 +5,22 @@ import time
 
 #take number of break from user 
 total_break =int(input("Enter the Numbers of break you wish to take in a day: "))
+
+#take the input from user about the time interval
+no_of_hours=int(input("Enter the time interval in which you wish to take break: "))
+
+time_in_sec=no_of_hours*60*60
+
 break_count = 0;
 
 #print current time
 
+
 print("The program started on "+time.ctime())
 while (break_count < total_break):
-    time.sleep(2*60*60);
-    webbrowser.open("https://www.youtube.com/watch?v=aTvJWKu9bMs&start_radio=1&list=RDaTvJWKu9bMs");
+    time.sleep(time_in_sec);
+    url="https://www.youtube.com/watch?v=aTvJWKu9bMs&start_radio=1&list=RDaTvJWKu9bMs"
+    webbrowser.open(url);
     break_count=break_count+1
 
 print("You completed all your breaks and current time is "+time.ctime())
